05b0c0882836c78cceef2174f3ce380d2e22c4c5
crvolosh <143448321+crvolosh@users.noreply.github.com>
2023-09-16 21:57:55 +0300

    k
    k

---
+++ javascript.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const container = document.querySelector(".container");
+  setTimeout(function () {
+    container.classList.add("show");
+  }, 500);
+});
+document.addEventListener("DOMContentLoaded", function () {
+  const container = document.querySelector(".container2");
+  setTimeout(function () {
+    container.classList.add("show");
+  }, 500);
+});
+document.addEventListener("DOMContentLoaded", function () {
+  const container = document.querySelector(".container3");
+  setTimeout(function () {
+    container.classList.add("show");
+  }, 500);
+});
